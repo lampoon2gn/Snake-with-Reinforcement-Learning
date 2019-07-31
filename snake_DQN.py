9a5f1e8ddf44cc667815515754b6870cdb90a8bb
Maohan Sun <maohan.sun@wsu.edu>
2019-07-31 17:56:05 -0600

    changed file names

--- snake_DQN.py
+++ snake_DQN.py
@@ -1,4 +1,3 @@
-import gym
 import numpy as np
 import random
 import math
@@ -8,7 +7,8 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
-env_name = 'CartPole-v0'
+from snake-game import *
+
 
 gamma =0.90
 learning_rate=0.0007
@@ -21,14 +21,14 @@ explore_decay=0.999
 
 class DQN:
 
-  def __init__(self,observation_space,action_space):
+  def __init__(self,input_dimension):
     self.exploration_rate = explore_max
-    self.action_space =action_space
+    self.action_list = [0,1]
     self.memory =deque(maxlen=mem_size)
 
     #NN stuff
     self.model = Sequential()
-    self.model.add(Dense(24, input_shape=(observation_space,), activation="relu"))
+    self.model.add(Dense(24, input_shape=(input_dimension,), activation="relu"))
     self.model.add(Dense(24, activation="relu"))
     self.model.add(Dense(24, activation="relu"))
     self.model.add(Dense(self.action_space, activation="linear"))
@@ -40,7 +40,7 @@ class DQN:
   #explore randomly or use NN to get an action
   def get_action(self,state):
     if np.random.rand() < self.exploration_rate:
-      return random.randrange(self.action_space)
+      return random.randrange(self.action_list)
     Q = self.model.predict(state)
     return np.argmax(Q[0])
 
@@ -59,32 +59,32 @@ class DQN:
     self.exploration_rate =max(explore_min,self.exploration_rate) 
 
 
-def cartpole():
-  env = gym.make(env_name)
+def snake_player():
+  snake-game.snake_game()
   #score_logger = ScoreLogger(env_name)
-  env._max_episode_steps= 200
-  observation_space =env.observation_space.shape[0]
-  action_space = env.action_space.n
-  solver = DQN(observation_space,action_space)
-  run = 0
-  while True:
-    run += 1
-    state = env.reset()
-    state = np.reshape(state,[1,observation_space])
-    step = 0 
-    while True:
-      step += 1
-      env.render()
-      action = solver.get_action(state)
-      state_next,reward,terminal,info = env.step(action)
-      reward = reward if not terminal else -reward
-      state_next = np.reshape(state_next, [1, observation_space])
-      solver.remember(state,action,reward,state_next,terminal)
-      state = state_next
-      if terminal:
-        print ("Run: " + str(run) + ", exploration: " + str(solver.exploration_rate) + ", score: " + str(step))
-        break
-      solver.experience_replay()
+  # env._max_episode_steps= 200
+  # observation_space =env.observation_space.shape[0]
+  # action_space = env.action_space.n
+  # solver = DQN(observation_space,action_space)
+  # run = 0
+  # while True:
+  #   run += 1
+  #   state = env.reset()
+  #   state = np.reshape(state,[1,observation_space])
+  #   step = 0 
+  #   while True:
+  #     step += 1
+  #     env.render()
+  #     action = solver.get_action(state)
+  #     state_next,reward,terminal,info = env.step(action)
+  #     reward = reward if not terminal else -reward
+  #     state_next = np.reshape(state_next, [1, observation_space])
+  #     solver.remember(state,action,reward,state_next,terminal)
+  #     state = state_next
+  #     if terminal:
+  #       print ("Run: " + str(run) + ", exploration: " + str(solver.exploration_rate) + ", score: " + str(step))
+  #       break
+  #     solver.experience_replay()
 
 if __name__ == "__main__":
-  cartpole()
+  snake_player()
