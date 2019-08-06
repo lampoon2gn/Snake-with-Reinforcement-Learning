e9d0bff3436e9c58511fba49670eca4ba92a25f6
Maohan Sun <maohan.sun@wsu.edu>
2019-08-05 22:37:27 -0600

    changed reward function

--- snake_game_text_based.py
+++ snake_game_text_based.py
@@ -3,6 +3,9 @@ import numpy as np
 import time
 import keyboard as kbd
 import os
+import matplotlib.cm as cm
+from matplotlib import pyplot as plt
+import matplotlib.animation as animation
 
 
 #use a list of (x,y) to represent snake
@@ -15,7 +18,7 @@ class snake():
     self.facing = self.__facing_options[ran] #initialize random facing
     self.got_apple = False
     self.num_of_moves = 0
-
+    self.reward = 0
   def apply_action(self,*args):
     '''find out what direction the snake will be facing after the action and move snake once'''
     if args:
@@ -57,6 +60,7 @@ class snake():
     '''add new cube in the direction the snake is facing, and chop off the tail(keep tail if got_apple)'''
     if self.got_apple:
       self.snake_list.insert(0,((self.snake_list[0][0]+int(self.facing[0]))%10,(self.snake_list[0][1]+int(self.facing[1]))%10))
+      self.num_of_moves = 0
       self.got_apple = False
     else:
       self.snake_list.insert(0,((self.snake_list[0][0]+int(self.facing[0]))%10,(self.snake_list[0][1]+int(self.facing[1]))%10))
@@ -86,12 +90,13 @@ def detect_collision(apple_object,snake_object):
   '''detect if snake ran into self/got apple'''
   snake_set = set(snake_object.snake_list) #use set to eliminate duplicate,duplicate means collision
   if len(snake_set) != len(snake_object.snake_list):
-    '''game over, return score'''
-    return len(snake_set)
+    '''game over'''
+    return True
   if apple_object.position in snake_object.snake_list:
     '''got an apple, keep tail next time snake moves,and generate new apple'''
     snake_object.got_apple = True
-    #todo: change the hard coded 19,19 to number of rows
+    snake_object.reward = 1
+
     apple_object.change_apple_position(snake_object)
 
 def draw_all(surface,snake_object,apple_object,dimension,rows):
@@ -109,7 +114,7 @@ class game():
   '''snake game'''
   GAME_GRID_DIMENSION = 10
   GAME_GRID_ROWS = 10
-  
+  flag = 1
   def __init__(self):
     #create game window
     self.game_window = np.zeros((self.GAME_GRID_ROWS,self.GAME_GRID_ROWS))
@@ -119,36 +124,52 @@ class game():
     self.a = apple(self.GAME_GRID_ROWS,self.GAME_GRID_ROWS,self.s)
     #terminal state
     self.terminal = False
-    self.reward = 0
 
 
   def get_env(self):
     '''return list(image),facing,reward,terminal'''
     data = self.game_window
-    return data,self.s.facing,len(self.s.snake_list)-1-self.s.num_of_moves*0.01,self.terminal
+    reward = self.s.reward
+    self.s.reward = 0
+    return data.flatten().reshape(1,100),self.s.facing,reward,self.terminal,len(self.s.snake_list)
+
+  def show_window(self):
+    lst = str(self.game_window)
+    lst = lst.replace('0',' ')
+    lst = lst.replace('1','x')
+    lst = lst.replace('0.5','O')
+    print(lst)
+
 
   def run_game(self,*args):
     '''run snake game for one frame.'''
-    while True:
-      self.game_window = np.zeros((self.GAME_GRID_ROWS,self.GAME_GRID_ROWS))
-      time.sleep(1)
-      self.terminal = False
-      if args:
-        self.s.apply_action(args[0])
-      else:
-        self.s.apply_action()
-      score = detect_collision(self.a,self.s)
-      #terminate condition
-      if score:
-        #print("Score: " + str(score))
-        self.s = snake(self.GAME_GRID_ROWS,self.GAME_GRID_ROWS)
-        self.terminal = True
-        score = 0
+    #while True:
+    self.game_window = np.zeros((self.GAME_GRID_ROWS,self.GAME_GRID_ROWS))
+    #time.sleep(1)
+    done = False
+    self.terminal = False
+    if args:
+      self.s.apply_action(args[0])
+    else:
+      self.s.apply_action()
+    draw_all(self.game_window,self.s,self.a,self.GAME_GRID_DIMENSION,self.GAME_GRID_ROWS)
+    done = detect_collision(self.a,self.s)
+    #terminate condition
+    if done:
+      #print("Score: " + str(score))
+      #self.s = snake(self.GAME_GRID_ROWS,self.GAME_GRID_ROWS)
+      self.terminal = True
+      
 
-      draw_all(self.game_window,self.s,self.a,self.GAME_GRID_DIMENSION,self.GAME_GRID_ROWS)
-      os.system('cls')
-      print(self.game_window)
+    
+    
+    #os.system('cls') 
+    #self.show_window()
+    #plt.pause(0.0001)
 
+
+    
+    #time.sleep(10)
 
 
 
